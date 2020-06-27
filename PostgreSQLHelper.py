046ab24fd3de8735f6f8959ac7fc485a63a7eb8c
mikolajgrzegorzek <mikgrzeg@icloud.com>
2020-06-27 12:09:55 +0200

    Extracting PostgreSQLHelper to a separate file. Creating tables if necessary. Useful details for sql quering added for every class.

---
+++ PostgreSQLHelper.py
@@ -0,0 +1,102 @@
+import psycopg2
+from psswd import PASSSWORD
+
+class PostgreSQLHelper:
+    """
+    Class with only static methods which perform many kind of operations with db.
+    """
+
+    @staticmethod
+    def create_table(table, columns,  parent_table=''):
+        """
+        Creating new table
+        """
+        try:
+            connection = psycopg2.connect(user="WindsurfingManagment", database="windsurfingmanagment",
+                                          host="localhost", password=PASSWORD)
+            cursor = connection.cursor()
+            create_table_query = ''' CREATE TABLE IF NOT EXISTS {0}
+                                    ({1})'''.format(table, columns)
+            if parent_table != '':
+                create_table_query += ' INHERITS ({0});'.format(parent_table)
+            else:
+                create_table_query += ';'
+            cursor.execute(create_table_query)
+            connection.commit()
+
+            print("Table created successfully in PostreSQL or it already exists.")
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+        finally:
+            if (connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed.")
+
+    @staticmethod
+    def insert_to_table(insert_query, record_to_insert):
+        """
+        Inserting record to table.
+        :arg insert_query - sql query
+        :arg record_to_insert - variables that will be saved
+        """
+        try:
+            connection = psycopg2.connect(user="WindsurfingManagment", database="windsurfingmanagment",
+                                          host="localhost", password=PASSWORD)
+            cursor = connection.cursor()
+            cursor.execute(insert_query, record_to_insert)
+
+            connection.commit()
+
+            print("Record inserted successfully into sail table")
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL", error)
+        finally:
+            if (connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed.")
+
+    @staticmethod
+    def bulk_insert(table, keywords, values):
+        """
+        Inserting multiple rows to a table
+        :param table: name of the table to be inserted
+        :param keywords: columns names
+        :param values: actual data to be inserted
+        """
+        try:
+            connection = psycopg2.connect(user="WindsurfingManagment",
+                                          password=PASSWORD,
+                                          host='localhost',
+                                          database='windsurfingmanagment')
+            cursor = connection.cursor()
+            sql_insert_query = ''' INSERT INTO {0} {1} VALUES {2}'''.format(table, keywords, values)
+            result = cursor.executemany(sql_insert_query)
+            connection.commit()
+            print(cursor.rowcount(), "Record inserted successfully into {0} table".format(table))
+        except (Exception, psycopg2.Error) as error:
+            print('Failed inserting record into ' + table + ' ' + error)
+        finally:
+            if (connection):
+                cursor.close()
+                connection.close()
+                print('PostgreSQL connection is closed')
+
+    @staticmethod
+    def update_table(id, model):
+        """
+        Method that updates records, fe. setting available attribute to the negation.
+        :param id: id of the item
+        :param model: model of the item (string)
+        """
+        pass
+
+    @staticmethod
+    def delete_data(id, model):
+        """
+        Method to remove record from db
+        :param id: id of the item
+        :param model: model of the item (string)
+        """
+        pass
--- WsEquipmentManagmentSystem.py
+++ WsEquipmentManagmentSystem.py
@@ -1,91 +1,24 @@
-import psycopg2
+from PostgreSQLHelper import PostgreSQLHelper
 import itertools
-from psswd import PASWORD
 
-class PostgreSQLHelper:
-    """
-    Class with only static methods which perform many kind of operations with db.
-    """
-    @staticmethod
-    def insert_to_table(insert_query, record_to_insert):
-        """
-        Inserting record to table.
-        :arg insert_query - sql query
-        :arg record_to_insert - variables that will be saved
-        """
-        try:
-            connection = psycopg2.connect(user="WindsurfingManagment", database="windsurfingmanagment",
-                                          host="localhost", password=PASSWORD)
-            cursor = connection.cursor()
-            cursor.execute(insert_query, record_to_insert)
-
-            connection.commit()
-
-            print("Record inserted successfully into sail table")
-        except (Exception, psycopg2.Error) as error:
-            print("Error while connecting to PostgreSQL", error)
-        finally:
-            if (connection):
-                cursor.close()
-                connection.close()
-                print("PostgreSQL connection is closed.")
-
-    @staticmethod
-    def bulk_insert(table, keywords, records):
-        """
-        Inserting multiple rows to a table
-        :param table: name of the table to be inserted
-        :param keywords: columns names
-        :param records: actual data to be inserted
-        """
-        try:
-            connection = psycopg2.connect(user="WindsurfingManagment",
-                                          password=PASSWORD)
-                                          host='localhost',
-                                          database='windsurfingmanagment')
-            cursor = connection.cursor()
-            sql_insert_query = ''' INSERT INTO {0} {1} VALUES {2}'''.format(table, keywords, records)
-            result = cursor.executemany(sql_insert_query)
-            connection.commit()
-            print(cursor.rowcount(), "Record inserted successfully into {0} table".format(table))
-        except (Exception, psycopg2.Error) as error:
-            print('Failed inserting record into ' + table + ' ' + error)
-        finally:
-            if(connection):
-                cursor.close()
-                connection.close()
-                print('PostgreSQL connection is closed')
-
-    @staticmethod
-    def update_table(id, model):
-        """
-        Method that updates records, fe. setting available attribute to the negation.
-        :param id: id of the item
-        :param model: model of the item (string)
-        """
-        pass
-
-    @staticmethod
-    def delete_data(id, model):
-        """
-        Method to remove record from db
-        :param id: id of the item
-        :param model: model of the item (string)
-        """
-        pass
+#TODO fix plain text db passwords
 
 
 class Equipment:
     """
     Base class for any kind of equipment
     """
-    newid = itertools.count()
+    sql_columns_details = '''ID serial PRIMARY KEY,
+                            MODEL TEXT NOT NULL,
+                            COST INT NOT NULL,
+                            AVAILABLE BOOLEAN NOT NULL'''
+    sql_columns = 'MODEL, COST, AVAILABLE'
+    table_name = 'equipment'
 
     def __init__(self, model_name, cost):
         self.model = model_name
         self.cost = cost
         self.available = True
-        #self.id = next(Equipment.newid)
 
     def __str__(self):
         return "Model: {0}\n\tCost:{1}".format(self.model, self.cost)
@@ -95,19 +28,19 @@ class Sail(Equipment):
     """
     Child class of Equipment which stands for sails.
     """
+    sql_columns_details = 'SIZE INT NOT NULL, CONDITION TEXT NOT NULL'
+    sql_columns = 'MODEL, COST, SIZE, CONDITION, AVAILABLE'
+    table_name = 'sail'
 
     def __init__(self, model_name, cost, size, condition):
         super(Sail, self).__init__(model_name=model_name, cost=cost)
         self.size = size
         self.condition = condition
-        self.insert_to_db()
+        #self.insert_to_db()
 
     def insert_to_db(self):
-        insert_query = ''' INSERT INTO sail 
-                                (MODEL,
-                                COST, SIZE,
-                                CONDITION, AVAILABLE)
-                                VALUES(%s,%s,%s,%s,%s)'''
+        insert_query = ''' INSERT INTO {0} ({1})
+                            VALUES(%s,%s,%s,%s,%s)'''.format(Sail.table_name, Sail.sql_columns)
         record_to_insert = (self.model, self.cost,
                             self.size, self.condition, self.available)
         PostgreSQLHelper.insert_to_table(insert_query=insert_query, record_to_insert=record_to_insert)
@@ -133,6 +66,9 @@ class Board(Equipment):
     Child class of equipment, that one is responsible for boards.
     During initialization, volume is being set.
     """
+    sql_columns_details = 'VOLUME INT NOT NULL'
+    sql_columns = 'MODEL, COST, VOLUME, AVAILABLE'
+    table_name = 'board'
 
     def __init__(self, volume, model_name, cost):
         super(Board, self).__init__(model_name=model_name, cost=cost)
@@ -144,7 +80,7 @@ class Board(Equipment):
     def broken_board(self):
         """
         If somebody somehow damage a board, and it needs to be repaired,
-        then availibility is set to False
+        then availability is set to False
         :return:
         """
         self.available = False
@@ -164,17 +100,25 @@ class DaggerBoard(Board):
     """
     Child class of Board, responsible for daggerboards.
     """
+    sql_columns_details = 'DAGGER BOOLEAN NOT NULL'
+    sql_columns = 'MODEL, COST, VOLUME, DAGGER, AVAILABLE'
+    table_name = 'dagger_board'
 
     def __init__(self, volume, model_name, cost):
         super().__init__(volume=volume, model_name=model_name, cost=cost)
         self.dagger = True
 
 
-class DaggerLessBoard(Board):
+class AdvancedBoard(Board):
     """
-    Child class of Board, responsible for daggerless boards.
-    During initialization few atributes are set.
+    Child class of Board, responsible for advanced boards.
+    During initialization few attributes are set.
     """
+    sql_columns_details = '''PURPOSE TEXT NOT NULL, 
+                            NOSE_PROTECTOR BOOLEAN NOT NULL,
+                            ANTYGRASS BOOLEAN NOT NULL'''
+    sql_columns = 'MODEL, COST, VOLUME, PURPOSE, NOSE_PROTECTOR, ANTYGRASS, AVAILABLE'
+    table_name = 'advanced_board'
 
     def __init__(self, if_antygrass, nose_protector, purpose, volume, model_name, cost):
         super().__init__(volume=volume, model_name=model_name, cost=cost)
@@ -186,41 +130,48 @@ class DaggerLessBoard(Board):
         return super().__str__() + "\n\tPurpose: {0}\n".format(self.purpose)
 
 
-#x = DaggerLessBoard(if_antygrass=False, nose_protector=False,
-                    #purpose='Slalom', volume=95, model_name='Goya Proton', cost=90)
+class HelpingTools:
+    @staticmethod
+    def name_of_parent_class_for_instance(object):
+        parent_class = type(object).__mro__[1].__name__.lower()
+        if parent_class != 'object':
+            return parent_class
+        else:
+            return ''
 
-print("{0}".format('balls'))
-#sail = Sail("Goya Mark", 70, 6.2, "As new")
-#Sail('Goya Mark', 70, 7.2, "As new")
+    @staticmethod
+    def name_of_parent_class_for_class(object):
+        parent_class = object.__mro__[1].__name__.lower()
+        if parent_class != 'object':
+            return parent_class
+        else:
+            return ''
+
+    @staticmethod
+    def create_tables_for_the_classes():
+        PostgreSQLHelper.create_table(Equipment.table_name, Equipment.sql_columns_details,
+                                      HelpingTools.name_of_parent_class_for_class(Equipment))
+        PostgreSQLHelper.create_table(Board.table_name, Board.sql_columns_details,
+                                      HelpingTools.name_of_parent_class_for_class(Board))
+        PostgreSQLHelper.create_table(DaggerBoard.table_name, DaggerBoard.sql_columns_details,
+                                      HelpingTools.name_of_parent_class_for_class(DaggerBoard))
+        PostgreSQLHelper.create_table(AdvancedBoard.table_name, AdvancedBoard.sql_columns_details,
+                                      HelpingTools.name_of_parent_class_for_class(AdvancedBoard))
+        PostgreSQLHelper.create_table(Sail.table_name, Sail.sql_columns_details,
+                                      HelpingTools.name_of_parent_class_for_class(Sail))
+
+
+if __name__ == "__main__":
+    not_exit = True
+    x = Sail('Goya Mark', 70, 6.2, 'As new')
+    HelpingTools.create_tables_for_the_classes()
+    #PostgreSQLHelper.bulk_insert(x.table_name, x.sql_columns, )
 """
-try:
-    connection = psycopg2.connect(user="WindsurfingManagment", database="windsurfingmanagment",
-                                  host="localhost", password=PASSWORD)
-    cursor = connection.cursor()
-    print("Connection established")
-        create_table_query = ''' CREATE TABLE SAIL
-        (ID INT PRIMARY KEY NOT NULL, 
-        MODEL TEXT NOT NULL, 
-        COST INT NOT NULL, 
-        SIZE REAL NOT NULL,
-        CONDITION TEXT,
-        AVAILABLE BOOLEAN NOT NULL);'''
-    cursor.execute('''INSERT INTO sail 
-                        (ID, MODEL,
-                        COST, SIZE,
-                        CONDITION, AVAILABLE)
-                        VALUES(%s,%s,%s,%s,%s,%s)''', (sail.id, sail.model, sail.cost,
-                                                       sail.size, sail.condition, sail.available))
-    connection.commit()
+    while not_exit:
+        str_input = input("Create an object:")
+        list_input = str_input.split(', ')
 
-    print("Record inserted successfully into sail table")
-    print("Table created successfully in PostreSQL")
+        if str_input == ':q':
+            not_exit = False
 
-except (Exception, psycopg2.Error) as error:
-    print("Error while connecting to PostgreSQL", error)
-finally:
-    if (connection):
-        cursor.close()
-        connection.close()
-        print("PostgreSQL connection is closed.")
 """
